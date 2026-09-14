0ea11e337f10d83d88312b4b426c868400a3efa4
prymania <prymania@gmail.com>
2026-09-14 17:17:13 +0700

    update config

--- 1-restaurant_system/config.py
+++ 1-restaurant_system/config.py
@@ -4,8 +4,10 @@
 #  ★ นิสิตต้องแก้ 4 บรรทัดนี้ให้เป็น user / password ของตัวเอง
 #    (ที่อาจารย์แจกให้สำหรับเชื่อมต่อ database server ของวิชา)
 # ============================================================
-DB_HOST = "your-db-host.university.ac.th"   # TODO: host ที่อาจารย์แจกให้
-DB_USER = "your_username"                     # TODO: username ของนิสิต
-DB_PASSWORD = "your_password"                 # TODO: password ของนิสิต
-DB_NAME = "your_database"                     # TODO: ชื่อฐานข้อมูลของนิสิต
+
+#สามารถ connect เป็น localhost ในเครื่องตัวเองได้
+DB_HOST = "202.28.34.202"               # TODO: host ที่อาจารย์แจกให้
+DB_USER = "s...."                     # TODO: username ของนิสิต
+DB_PASSWORD = "s....pwd"                 # TODO: password ของนิสิต
+DB_NAME = "prymania_s...."              # TODO: ชื่อฐานข้อมูลของนิสิต
 DB_PORT = 3306
