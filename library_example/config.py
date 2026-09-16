a751181783302afc54a3ca5414bb26d865882a48
prymania <prymania@gmail.com>
2026-09-16 23:07:26 +0700

    Convert library_example to regular folder

---
+++ library_example/config.py
@@ -0,0 +1,19 @@
+# ============================================================
+#  config.py  —  ตั้งค่าการเชื่อมต่อฐานข้อมูล MySQL
+# ============================================================
+#  ★ นิสิตต้องแก้ 4 บรรทัดนี้ให้เป็น user / password ของตัวเอง
+#    (ที่อาจารย์แจกให้สำหรับเชื่อมต่อ database server ของวิชา)
+# ============================================================
+
+#สามารถ connect เป็น localhost ในเครื่องตัวเองได้
+# DB_HOST = "202.28.34.202"               # TODO: host ที่อาจารย์แจกให้
+# DB_USER = "demo01"                     # TODO: username ของนิสิต
+# DB_PASSWORD = "demo01"                 # TODO: password ของนิสิต
+# DB_NAME = "prymania_demo01"              # TODO: ชื่อฐานข้อมูลของนิสิต
+# DB_PORT = 3306
+
+DB_HOST = "localhost"               # TODO: host ที่อาจารย์แจกให้
+DB_USER = "root"                     # TODO: username ของนิสิต
+DB_PASSWORD = "abcd1234"                 # TODO: password ของนิสิต
+DB_NAME = "project69"              # TODO: ชื่อฐานข้อมูลของนิสิต
+DB_PORT = 3306
