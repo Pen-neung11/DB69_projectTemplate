948ba3b9efe828f3d4bd07a7abd42703a867edd0
ธนโชติ ทองพู <68011218017@msu.ac.th>
2026-09-25 18:21:01 +0700

    ทำส่วน 2-course_systempy

--- 2-course_system/config.py
+++ 2-course_system/config.py
@@ -6,8 +6,9 @@
 # ============================================================
 
 #สามารถ connect เป็น localhost ในเครื่องตัวเองได้
-DB_HOST = "202.28.34.202"               # TODO: host ที่อาจารย์แจกให้
-DB_USER = "s...."                     # TODO: username ของนิสิต
-DB_PASSWORD = "s....pwd"                 # TODO: password ของนิสิต
-DB_NAME = "prymania_s...."              # TODO: ชื่อฐานข้อมูลของนิสิต
+DB_HOST = "localhost"               # TODO: host ที่อาจารย์แจกให้
+DB_USER = "root"                     # TODO: username ของนิสิต
+DB_PASSWORD = "abcd1234"                 # TODO: password ของนิสิต
+DB_NAME = "project69"              # TODO: ชื่อฐานข้อมูลของนิสิต
 DB_PORT = 3306
+
